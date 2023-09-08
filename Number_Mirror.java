925004cfeaa4c449f697a1c9f907604b923dc113
VishalWaje <vishalwaje09@gmail.com>
2023-09-08 20:31:27 +0530

    Initial commit

---
+++ Number_Mirror.java
@@ -0,0 +1,24 @@
+/* Write a program that takes a number 
+�
+N as the input, and prints it to the output.
+
+Input Format
+The only line of input contains a single integer.
+
+Output Format
+Output the answer in a single line. 
+Sample Input: 123
+Sample output: 123
+*/
+
+import java.util.*;
+
+class Number_Mirror {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Number Number");
+            int num = sc.nextInt();
+            System.out.println("Your Number is:" + num);
+        }
+    }
+}
