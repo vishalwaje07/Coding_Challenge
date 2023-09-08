3879a8d79039621391ce8ee6393cb06942fa849d
VishalWaje <vishalwaje09@gmail.com>
2023-09-08 21:18:24 +0530

    first program

---
+++ Number_Mirror.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Number_Mirror {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int num = sc.nextInt();
+            System.out.println(num);
+        }
+    }
+
+}
