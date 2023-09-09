95c4b25bb7407d0b591246d3d80ee2fea04899c0
VishalWaje <vishalwaje09@gmail.com>
2023-09-09 18:36:47 +0530

    Second day

--- Fibonacciwithoutrecursion.java
+++ Fibonacciwithoutrecursion.java
@@ -1,7 +1,7 @@
 /* fibonacci series, next number is the sum of previous two 
 numbers for example 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 etc. 
 The first two numbers of fibonacci series are 0 and 1 */
-public class Fibonacciwithoutrecursion {
+public class FibonacciWithoutRecursion {
     public static void main(String[] args) {
         int num1 = 0;
         int num2 = 1;
