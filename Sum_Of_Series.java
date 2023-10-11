4e418319077bc386a9e0dcf959878a95a60f6302
VishalWaje <vishalwaje09@gmail.com>
2023-10-11 23:01:53 +0530

    10th Day

---
+++ Sum_Of_Series.java
@@ -0,0 +1,14 @@
+/*Write a program to find the sum of the given series 
+1+2+3+ . . . . . .(N terms)  */
+import java.util.*;
+public class Sum_Of_Series {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Numbers");
+        int nums = sc.nextInt(); 
+        for (int i= 0; i<nums; i++){
+            int numbers = sc.nextInt();
+        
+        }
+    }
+}
