ae5bb9356b8c3e9a712adb2ffb4c51df2ba52e13
VishalWaje <vishalwaje09@gmail.com>
2023-12-17 12:29:08 +0530

    Commit

--- Code Again/PrimeNumber.java
+++ Code Again/PrimeNumber.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 public class PrimeNumber {
     public static void main(String args[]) {
-        System.out.println("Enter Number");
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
         int number = sc.nextInt();
         int count = 0;
 
-        if (number >= 1) {
-            for (int i = 2; i <= number; i++) {
+        if (number > 1) {
+            for (int i = 1; i < number; i++) {
                 if (number % i == 0) {
                     count++;
                 }
             }
-            if (count > 1) {
-                System.out.println(number + "Is not a prime Number");
-            } 
-            else {
-                System.out.println(number + "Is  a prime number");
+            if (count < 1) {
+                System.out.println(number + " is a prime number");
+            } else {
+                System.out.println(number + "is a prime number");
             }
 
         } else {
-            System.out.println(number + "Is Not a prime number");
+            System.out.println(number + "is not a prime number");
         }
+
     }
 }
