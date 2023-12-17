1b39abba448fac0bc958a19316c3808f03538537
VishalWaje <vishalwaje09@gmail.com>
2023-12-17 12:32:31 +0530

    Commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Hello {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter anything");
+        String word = sc.nextLine();
+        System.out.println("You have entred :" + word);
+
+    }
+}
