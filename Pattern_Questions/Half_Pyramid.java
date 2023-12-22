be66878004801808e421d73223d32f1052c704d8
VishalWaje <vishalwaje09@gmail.com>
2023-12-22 22:03:46 +0530

    Commit

---
+++ Pattern_Questions/Half_Pyramid.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Half_Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int number = sc.nextInt();
+
+        // Outer Loop
+        for (int i = 1; i <= number; i++) {
+            // Inner Loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
