019f77193ab6afe203cda4891b4779b6f5a36217
VishalWaje <vishalwaje09@gmail.com>
2024-03-14 16:50:00 +0530

    Commit

--- Loops/Table.java
+++ Loops/Table.java
@@ -1,11 +1,12 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Table {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
         int number = sc.nextInt();
 
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(number * i);
         }
     }
