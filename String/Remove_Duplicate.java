d3fcdbd75732579d26083eab05b98fd120533ddb
VishalWaje <vishalwaje09@gmail.com>
2024-03-22 17:57:42 +0530

    Commit

--- String/Remove_Duplicate.java
+++ String/Remove_Duplicate.java
@@ -6,8 +6,10 @@ public class Remove_Duplicate {
         System.out.println("Enter String ");
         String str = sc.nextLine();
 
-        
+        StringBuilder sb1 = new StringBuilder();
 
+        str.chars().distinct().forEach(c -> sb1.append((char) c));
+        System.out.println(sb1);
 
     }
 
