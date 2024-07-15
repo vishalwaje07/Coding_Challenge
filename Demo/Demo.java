524c1de8ff9526618dc84772307f8f562c306299
VishalWaje <vishalwaje09@gmail.com>
2024-07-15 09:39:52 +0530

    sql file emp

--- Demo/Demo.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("Learning Git");
-
-    }
-}
---
+++ Demo/empfile.sql
@@ -0,0 +1,1 @@
+create table emp_info ( id int, name varchar(60))
