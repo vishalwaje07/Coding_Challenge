51f854a3d438f2a1ad41e46e89d0e86253b2f76a
VishalWaje <vishalwaje09@gmail.com>
2024-08-08 19:16:34 +0530

    Commit

---
+++ Advance_Java/spring_boot_starting/src/main/java/com/springboot/web/SpringBootStartingApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStartingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStartingApplication.class, args);
+	}
+
+}
