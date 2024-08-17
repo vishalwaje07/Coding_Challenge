73ab0966f829975187ebb5de4576df88a8a288d4
VishalWaje <vishalwaje09@gmail.com>
2024-08-17 09:39:55 +0530

    Commit

--- Advance_Java/thymeleafPractical/src/main/java/com/practice/controller/MyController.java
+++ Advance_Java/thymeleafPractical/src/main/java/com/practice/controller/MyController.java
@@ -1,6 +1,11 @@
 package com.practice.controller;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,9 +13,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MyController {
 
 	@RequestMapping(value = "/about", method = RequestMethod.GET)
-public String about() {
-System.out.println("Inside the about...");
-return "about";
+	public String about(Model model) {
+		System.out.println("Inside the about...");
+
+		// Putting Data in model
+		model.addAttribute("name", "vishal waje".toUpperCase());
+		model.addAttribute("currentDate", new Date().toLocaleString());
+		// Example
+//		String name = "Vishal";
+//		name.toUpperCase();
+
+		return "about";
 		// about.html
 	}
+
+	// This is for Iteration Handeling
+
+	@GetMapping("/example-loop")
+	public String iterateHandlar(Model m) {
+		// Create a list, set collection...
+		List<String> names = List.of("Vishal", "Nikhil", "Sarthak", "Ashish");
+		m.addAttribute("names", names);		
+		return "iterate";
+	}
+
 }
