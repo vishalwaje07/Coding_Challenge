4209286b45a83beb9f9d4bb123d60de5cecdd165
VishalWaje <vishalwaje09@gmail.com>
2024-08-22 18:29:58 +0530

    Commit

--- Advance_Java/springvalidation/src/main/java/com/validate/controller/MyController.java
+++ Advance_Java/springvalidation/src/main/java/com/validate/controller/MyController.java
@@ -2,16 +2,34 @@ package com.validate.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.validate.entities.LoginData;
 
+import jakarta.validation.Valid;
+
 @Controller
 public class MyController {
-	
+
 	@GetMapping("/form")
 	public String openForm(Model model) {
+		System.out.println("Opening Form");
 		model.addAttribute("loginData", new LoginData());
 		return "form";
+	}
+
+	// Handeler for processing the data
+	@PostMapping("/process")
+	public String processForm(@Valid @ModelAttribute("loginData") LoginData loginData, BindingResult result ) {
+		if(result.hasErrors()) {
+			System.out.println(result);
+			return "form";
+		}
+		
+		System.out.println(loginData);
+		return "success";
 	}
 }
--- Advance_Java/springvalidation/src/main/java/com/validate/entities/LoginData.java
+++ Advance_Java/springvalidation/src/main/java/com/validate/entities/LoginData.java
@@ -1,19 +1,35 @@
 package com.validate.entities;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class LoginData {
-	private String  userName;
+
+	@NotBlank(message = "User name can not be blank")
+	@Size(min = 3, max = 12, message = "User name must be in 3 -12 ")
+	private String userName;
+
 	private String email;
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginData [userName=" + userName + ", email=" + email + "]";
 	}
 
 }
