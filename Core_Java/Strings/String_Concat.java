da943d30e6323b87b1610f2d8d2730a7a3ac98fb
VishalWaje <vishalwaje09@gmail.com>
2024-11-07 11:07:34 +0530

    Commit

---
+++ Core_Java/Strings/String_Concat.java
@@ -0,0 +1,11 @@
+public class String_Concat {
+    public static void main(String[] args) {
+        String firstName = "Vishal";
+        String lastName = "Waje";
+
+        // System.out.println(firstName + " " + lastName);
+        // System.out.println(firstName.concat(lastName));
+        System.out.println(firstName.concat(" ").concat(lastName));
+    }
+
+}
