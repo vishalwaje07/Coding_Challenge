ad967c038b9ccbd73c3c46a7bd32cd9a61e63b3e
VishalWaje <vishalwaje09@gmail.com>
2024-11-07 11:26:13 +0530

    Commit

--- Core_Java/Strings/String_Concat.java
+++ Core_Java/Strings/String_Concat.java
@@ -1,11 +1,18 @@
 public class String_Concat {
+    // Any String operation tries to modify then it will give us new String object.
     public static void main(String[] args) {
         String firstName = "Vishal";
         String lastName = "Waje";
 
+        String str1 = "Hello";
+        str1 = str1 + " " + "World";
+
+        System.out.println(str1);
+
         // System.out.println(firstName + " " + lastName);
+        // System.out.println(firstName + lastName);
         // System.out.println(firstName.concat(lastName));
-        System.out.println(firstName.concat(" ").concat(lastName));
+        // System.out.println(firstName.concat(" ").concat(lastName));
     }
 
 }
