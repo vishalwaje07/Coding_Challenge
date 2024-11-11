6692bb6c6aa7d80656a27b37e08c8209a59a7593
VishalWaje <vishalwaje09@gmail.com>
2024-11-11 10:43:39 +0530

    Commit

--- Core_Java/Array/ArrayElement.java
+++ Core_Java/Array/ArrayElement.java
@@ -1,6 +1,20 @@
-public class MaxSubArray{
+public class ArrayElement {
     // Array element is present or not.
     public static void main(String[] args) {
-        int [] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int target = 7;
+
+        boolean flag = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag == true) {
+            System.out.println("Element Found");
+        } else {
+            System.out.println("Element Not Found");
+        }
     }
 }
