05160c832ee38bf880a46ae9c1d43373148bc687
VishalWaje <vishalwaje09@gmail.com>
2024-11-12 11:12:19 +0530

    Commit

--- Core_Java/Streams/Reverse_String_All.java
+++ Core_Java/Streams/Reverse_String_All.java
@@ -1,9 +1,21 @@
 public class Reverse_String_All {
     public static void main(String[] args) {
-        String originalString = "Vishal Waje";
-        StringBuilder sb = new StringBuilder(originalString);
-        String revesed = sb.reverse().toString();
-        System.out.println(revesed);
-    }
+        /*
+         * By using the String Builder
+         * String originalString = "Vishal Waje";
+         * StringBuilder sb = new StringBuilder(originalString);
+         * String reversed = sb.reverse().toString();
+         * System.out.println(reversed);
+         */
 
+        /*
+         * By using the for loop
+         * String originalString = "Vishal Waje";
+         * String reversed = " ";
+         * for (int i = originalString.length() - 1; i >= 0; i--) {
+         * reversed = reversed + originalString.charAt(i);
+         * }
+         * System.out.println(reversed);
+         */
+    }
 }
