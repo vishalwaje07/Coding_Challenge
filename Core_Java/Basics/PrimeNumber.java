5a5ec7358e50ae466d5355144497f0aaa68c45d0
VishalWaje <vishalwaje09@gmail.com>
2024-11-19 12:55:31 +0530

    Commit

---
+++ Core_Java/Basics/PrimeNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = sc.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + "Number is prime");
+        } else {
+            System.out.println(number + "Number is not prime");
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
