0e50db3f462c57911ab2972f6acce76ecb6958e4
VishalWaje <vishalwaje09@gmail.com>
2024-12-24 15:03:02 +0530

    Commit

--- Advance_Java/curd-operations/src/main/java/controller/UserController.java
+++ Advance_Java/curd-operations/src/main/java/controller/UserController.java
@@ -1,9 +1,12 @@
 package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import entity.User;
 import repository.UserRepository;
 
 @RestController
@@ -16,6 +19,10 @@ public class UserController {
 	
 	// Get Request
 	// Post Request
+	@PostMapping
+	public User createOrUpdateUser(@RequestBody) {
+		
+	}
 	// Put Request
 	// Delete Request
 
